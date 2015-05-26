e18f8cf952be99e5c97615f5ce5475fdebae782c
Richard Shea <rshea@thecubagroup.com>
2015-05-26 20:41:38 +1200

    Allow for updating of Bears (via PUT)

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Track items you want to do every day - implemented in Node
 The initial parts of this are based on ideas found here : 
 
 https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
+
+
+# Remember
+
+When doing insert/update/delete the form has to be submitted as application/x-www-form-urlencoded.
+
+Remember the HTTP verbs for a:
+
+INSERT is POST
+DELETE is DELETE
+UPDATE is PUT
+DISPLAY is GET
+LIST is GET
+
+
--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 // server.js
-
 // // BASE SETUP
 // // =============================================================================
 //
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost/assidu_node')
 var Bear     = require('./app/models/bear');
-
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 var port = process.env.PORT || 8080;        // set our port
-
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
 // all access route
 router.use(function(req, res, next) {
     // do logging
@@ -38,6 +32,7 @@ router.get('/', function(req, res) {
 // ----------------------------------------------------
 router.route('/bears')
     .post(function(req, res) {
+        //Insert a Bear            
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
         // save the bear and check for errors
@@ -59,7 +54,9 @@ router.route('/bears')
 // on routes that end in /bears/:bear_id
 // ----------------------------------------------------
 router.route('/bears/:bear_id')
-    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    // get the bear with that id 
+    // (accessed at GET 
+    // http://localhost:8080/api/bears/:bear_id)
     .get(function(req, res) {
         console.log("Fetching the Bear: " + req.params.bear_id)
         Bear.findById(req.params.bear_id, function(err, bear) {
@@ -69,6 +66,24 @@ router.route('/bears/:bear_id')
             res.json(bear);
         });
     })
+    // update a specific bear - **** it's a PUT not a POST **** //
+    .put(function(req, res) {
+        console.log("Updating the Bear: " + req.params.bear_id)
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err){
+                res.send(err);
+            }
+            bear.name = req.body.name;  // update the bears info
+            // save the bear
+            bear.save(function(err) {
+                if (err){
+                    res.send(err);
+                }
+                res.json({ message: 'Bear updated!' });
+            });
+        });
+    })
+    // delete a specific bear
     .delete(function(req, res) {
         console.log("Deleting the Bear: " + req.params.bear_id)
         Bear.remove({
@@ -81,7 +96,6 @@ router.route('/bears/:bear_id')
         });
     });
 // REGISTER OUR ROUTES -------------------------------
-
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 // START THE SERVER
