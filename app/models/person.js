902a28e613357c727d34db1b0915db43d3231bc4
Richard Shea <rshea@thecubagroup.com>
2015-06-16 19:11:42 +1200

    Introduce Person schema

---
+++ app/models/person.js
@@ -0,0 +1,8 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var PersonSchema   = new Schema({
+        name: String
+});
+
+module.exports = mongoose.model('Person', PersonSchema);
