840b2040ce81f235da990aa9a9af51cda71f632f
Richard Shea <rshea@thecubagroup.com>
2015-10-13 20:43:36 +1300

    Now able to commit/delete and automatically see changed data on the list screen !

--- testrig/js/assidu-node.js
+++ testrig/js/assidu-node.js
@@ -4,23 +4,26 @@ function mainController($scope, $http) {
     $scope.formData = {};
 
     // when landing on the page, get all persons and show them
-    $http.get('http://localhost:8080/api/persons')
-        .success(function(data) {
-            $scope.persons = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
 
+    $scope.initFirst = function() {
+        console.log("Here we are in initFirst");
+        $http.get('http://localhost:8080/api/persons')
+            .success(function(data) {
+                $scope.persons = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
     // when submitting the add form, send the text to the node API
     $scope.createPerson = function() {
         $http.post('http://localhost:8080/api/persons', $scope.formData)
             .success(function(data) {
                 $scope.formData = {}; // clear the form so our user is ready to enter another
                 $scope.persons = data;
-                console.log("createPerson in assidu-node.js - SUCCESS");
                 console.log(data);
+                $scope.initFirst();
             })
             .error(function(data) {
                 console.log('Error: ' + data);
@@ -32,6 +35,7 @@ function mainController($scope, $http) {
             .success(function(data) {
                 $scope.persons = data;
                 console.log(data);
+                $scope.initFirst();
             })
             .error(function(data) {
                 console.log('Error: ' + data);
